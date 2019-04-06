c81ee6075c8f3e81eb94782a50afd69ddeb557b7
Craig Calvert <ccalvert@csumb.edu>
2019-04-05 18:32:36 -0700

    Initial Assign5.java file
    Rough start to phase 1.

---
+++ CST 338 Software Design/Module 5/Assign5.java
@@ -0,0 +1,86 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Assign5
+{
+   // static for the 57 icons and their corresponding labels
+   // normally we would not have a separate label for each card, but
+   // if we want to display all at once using labels, we need to.
+
+   static final int NUM_CARD_IMAGES = 57; // 52 + 4 jokers + 1 back-of-card image
+   static Icon[] icon = new ImageIcon[NUM_CARD_IMAGES];
+
+   static void loadCardIcons()
+   {
+      // Builds the file names ("AC.gif", "2C.gif", "3C.gif", "TC.gif", etc.)
+      // in a SHORT loop.  For each file name, read it in and use it to
+      // instantiate each of the 57 Icons in the icon[] array.
+
+      String suit = "";
+      String cardValue = "";
+      ImageIcon cardIcon = new ImageIcon();
+
+      for (int i = 0; i < icon.length - 1; i++)
+      {
+         for (int j = 0; j < 4; j++)
+         {
+            suit = turnIntIntoCardSuit(j);
+
+            for (int k = 0; k < 14; k++)
+            {
+               ;
+               cardValue = turnIntIntoCardValue(k);
+               icon[i] = cardIcon;
+               i++;
+            }
+         }
+      }
+   }
+
+   // turns 0 - 13 into "A", "2", "3", ... "Q", "K", "X"
+   static String turnIntIntoCardValue(int k)
+   {
+      String[] cardValue = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T",
+            "J", "Q", "K", "X"};
+      return cardValue[k];
+   }
+
+   // turns 0 - 3 into "C", "D", "H", "S"
+   static String turnIntIntoCardSuit(int j)
+   {
+      String[] suit = {"C", "D", "H", "S"};
+      return suit[j];
+   }
+
+   // a simple main to throw all the JLabels out there for the world to see
+   public static void main(String[] args)
+   {
+      int k;
+
+      // prepare the image icon array
+      loadCardIcons();
+
+      // establish main frame in which program will run
+      JFrame frmMyWindow = new JFrame("Card Room");
+      frmMyWindow.setSize(1150, 650);
+      frmMyWindow.setLocationRelativeTo(null);
+      frmMyWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      // set up layout which will control placement of buttons, etc.
+      FlowLayout layout = new FlowLayout(FlowLayout.CENTER, 5, 20);
+      frmMyWindow.setLayout(layout);
+
+      // prepare the image label array
+      JLabel[] labels = new JLabel[NUM_CARD_IMAGES];
+      for (k = 0; k < NUM_CARD_IMAGES; k++)
+         labels[k] = new JLabel(icon[k]);
+
+      // place your 3 controls into frame
+      for (k = 0; k < NUM_CARD_IMAGES; k++)
+         frmMyWindow.add(labels[k]);
+
+      // show everything to the user
+      frmMyWindow.setVisible(true);
+   }
+}
+
