d5114321e990ccb77bc4f5e3f68e9ca8f23d8d42
Craig Calvert <ccalvert@csumb.edu>
2019-08-02 12:11:56 -0700

    added page where user can create an account

--- CST 336 Internet Design/cst336-final/app.js
+++ CST 336 Internet Design/cst336-final/app.js
@@ -22,6 +22,10 @@ app.get("/login", function(req, res) {
    res.render("login.ejs");
 })
 
+app.get("/signUp", function(req, res) {
+   res.render("signUp.ejs");
+})
+
 app.get("/dataEntry", function(req, res) {
    res.render("dataEntry.ejs");
 })
